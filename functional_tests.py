251f0da7d5b74f35e0cca0b9c140c9ec8f413af8
Bill McMillin <billmcmillin@gmail.com>
2018-03-20 13:40:18 -0400

    add templates

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,6 +16,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # User navigates to homepage and sees correct title
         self.assertIn('Batchcave', self.browser.title)
+
+        # user is presented with options for creating a new conversion or
+        # viewing past conversions
+        select_menu = self.browser.find_element_by_id('id_create_new')
+        self.assertEqual(
+            select_menu.get_attribute('placeholder'),
+            'Create New Conversion'
+        )
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('BatchCave', header_text)
+
+        #User can choose to start a new conversion
+        first_choice = self.browser.find_element_by_id('id_menu_createConversion')
+        self.assertIn('Create New Conversion', first_choice.text)
+        #User can view an index of past conversions
+        second_choice = self.browser.find_element_by_id('id_menu_indexConversion')
+        self.assertIn('View Past Conversions', second_choice.text)
         self.fail("finish me!")
 
 if __name__ == '__main__':
